2dafd874d62b0f021b95e9647bca9cc57edc4649
KayDrew <kdtshehla@gmail.com>
2023-07-23 20:11:24 +0200

    added pay functionality and past orders

--- js/index.js
+++ js/index.js
@@ -10,32 +10,45 @@ document.addEventListener("alpine:init", () => {
                   cartPizzas:[],
                   cartTotal:0,
                   username: "KayDrew",
-                  cartId: "",
+                  pizzaId: "",
               showCart:false,
-                  pizzaId:"",
+                  cartId:"",
                   showMenu:true,
                   showHistory:false,
+                  pastOrders:[],
 showCheckout:false,
-showInput:false,                  
+showInput:false, 
+message:"",
+amount:0,                 
                   
 
             
+createCart(){
 
+ axios.get('https://pizza-api.projectcodex.net/api/pizza-cart/create?username=${this.username}').then(result=>{
+
+this.cartId=result.data.cart_code;
+
+
+});
+
+
+
+},
 
 
             getCart(){
-
-            
-
-                  return axios.get("https://pizza-api.projectcodex.net/api/pizza-cart/skFkDG1DC1/get");
+                  return axios.get('https://pizza-api.projectcodex.net/api/pizza-cart/'+this.cartId+'/get');
             },
 
 
 
             //increment cart
             buy(pizzaId){
+            	
+            
 
-                  axios.post('https://pizza-api.projectcodex.net/api/pizza-cart/add', { 'cart_code': 'skFkDG1DC1', 
+                  axios.post('https://pizza-api.projectcodex.net/api/pizza-cart/add', { 'cart_code': this.cartId, 
                   'pizza_id': pizzaId}).then(
 
                         () => {
@@ -46,7 +59,8 @@ showInput:false,
                         this.cartPizzas=result.data.pizzas;
                         this.cartTotal=result.data.total.toFixed(2);
                          this.showCheckout=true;
-                     
+                         
+                                       
                          
                         }
                   
@@ -73,29 +87,37 @@ showInput:false,
 this.showInput=true;
 },
 
-            pay(pizzaId){
-            	
-axios.post('https://pizza-api.projectcodex.net/api/pizza-cart/pay',{'cart_code':'skFkDG1DC1',
-'pizza_id': pizzaId}).then(()=>{
+            pay(){
+      
+      
+var paymentAmount=Number(this.amount);  	
+axios.post('https://pizza-api.projectcodex.net/api/pizza-cart/pay',{'cart_code':this.cartId,
+'amount': paymentAmount}).then((result)=>{
 
+                  if(result. data.status=="failure"){
+this.message=result.data.message;
+}
 
-                  this.error = false;
-                  this.orderItems = false;
+else{
                   this.showInput = false;
                   this.showCheckout = false;
                   
+                 this.message="Payment successful!";
 
-this.getCart().then(result=> {
+ 
+                        this.pastOrders=this.cartPizzas;
 
-                        this.cartPizzas=result.data.pizzas;
-                        this.cartTotal=result.data.total.toFixed(2);
-                        
-                      
-                        }
+console.log(this.pastOrders);
+ 
+              
+
+this.cartPizzas=[];
+this.cartTotal=0;
+
+this.createCart();
                   
-
-
-                  );
+                  
+                  }
 
  
 });
@@ -109,6 +131,9 @@ this.getCart().then(result=> {
 this.showCart=true;
 this.showMenu=false;
 this.showHistory=false;
+this.showCheckout=true;
+
+
 }
 
 
@@ -144,7 +169,7 @@ this.showHistory=false;
 
 add(pizzaId){
 
-axios.post('https://pizza-api.projectcodex.net/api/pizza-cart/add', { 'cart_code': 'skFkDG1DC1', 
+axios.post('https://pizza-api.projectcodex.net/api/pizza-cart/add', { 'cart_code': this.cartId, 
                   'pizza_id': pizzaId}).then(
 
                         () => {
@@ -177,7 +202,7 @@ axios.post('https://pizza-api.projectcodex.net/api/pizza-cart/add', { 'cart_code
 
 remove(pizzaId){
 
-axios.post('https://pizza-api.projectcodex.net/api/pizza-cart/remove', { 'cart_code': 'skFkDG1DC1', 
+axios.post('https://pizza-api.projectcodex.net/api/pizza-cart/remove', { 'cart_code': this.cartId, 
                   'pizza_id': pizzaId}).then(
 
                         () => {
@@ -190,6 +215,7 @@ axios.post('https://pizza-api.projectcodex.net/api/pizza-cart/remove', { 'cart_c
                         
                         if(this.cartTotal==0){
 this.showCheckout=false;
+this.showInput=false;
 
 }       
 
@@ -222,19 +248,19 @@ this.showCheckout=true;
                         axios.get('https://pizza-api.projectcodex.net/api/pizzas').then(result => {
 
                               this.pizzas = result.data.pizzas;
-                              console.log(this.pizzas);
+                              
                               this.smallPizza = this.pizzas[16];
+                              
+                           this.createCart();  
 
+this.getCart().then(result=>{
 
-                              this.getCart().then(result => {
+this.cartPizzas =result.data.pizzas;
+this.cartTotal=result.data.total;
 
-                                    this.cartPizzas = result.data.pizzas;
-                                    this.cartTotal=this.cartPizzas.total;
-                                    console.log(this.cartPizzas);
-
-
-                              });
-
+}
+);
+                    
                         }
                         );
 
